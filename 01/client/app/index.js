693eba6ac113fce32d2d30d70c503958e5104a51
Luis Antonio <luis.santiago@mblabs.com.br>
2024-02-28 22:58:16 -0500

    restore client cap 1

--- 01/client/app/index.js
+++ 01/client/app/index.js
@@ -1,17 +1,33 @@
-const fields = [
-    document.querySelector('#date'),
-    document.querySelector('#quantity'),
-    document.querySelector('#value')
-]
 
-// # .
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#valor'),
+    document.querySelector('#quantidade')
+];
 
-function onSubmit(event) {
-    event.preventDefault() // page are not refreshing
+console.log(campos);
 
-    console.log(fields[0].value)
-    console.log(fields[1].value)
-    console.log(fields[2].value)
-}
+var tbody = document.querySelector('table tbody');
 
-document.querySelector('.form').addEventListener('submit', onSubmit)
+document.querySelector('.form').addEventListener('submit', function (event) {
+
+    event.preventDefault();
+    var tr = document.createElement('tr');
+
+    campos.forEach(function (campo) {
+        var td = document.createElement('td');
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
+
+    var tdVolume = document.createElement('td');
+    tdVolume.textContent = campos[1].value * campos[2].value;
+    tr.appendChild(tdVolume);
+
+    tbody.appendChild(tr);
+
+    campos[0].value = '';
+    campos[1].value = 1;
+    campos[2].value = 0;
+    campos[0].focus();
+});
